9ff53c3981b1656fcc39c4e9159d31b7237f2efb
Allie Neal <allieneal93@gmail.com>
2021-03-05 09:44:51 -0500

    adds validations to actors

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,7 @@
 class Actor < ApplicationRecord
+  validates :actor, :first_name, length: {minimum: 2}
+  validates :actor, :last_name, length: {minimum: 2}
+  validates :actor, :known_for, presence: true
+  validates :actor, :age, numericality: {greater_than: 13}
+  
 end
--- db/migrate/20210303225018_adds_gender_and_age_to_actors.rb
+++
@@ -1,6 +0,0 @@
-class AddsGenderAndAgeToActors < ActiveRecord::Migration[6.1]
-  def change
-    adds_column :actors, :gender, :string
-    adds_column :actors, :age, :integer
-  end
-end
