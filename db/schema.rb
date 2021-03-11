1d943d8d544844012c6af85e2e12e1d13c8ac7c1
Allie Neal <allieneal93@gmail.com>
2021-03-11 17:38:24 -0500

    adds users and sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_153548) do
+ActiveRecord::Schema.define(version: 2021_03_11_223710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2021_03_09_153548) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "director"
     t.boolean "english", default: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-actor = Actor.create!({first_name: "Leonardo", last_name: "Dicaprio", known_for: "Titanic", gender: "male", age: 46})
+# actor = Actor.create!({first_name: "Leonardo", last_name: "Dicaprio", known_for: "Titanic", gender: "male", age: 46})
 
-movie = Movie.create!({title: "The Conjuring", year: 2013, plot: "When the Perron family movies into a new house, they quickly start experiencing paranormal happenings. Things keep getting worse, so mom Carolyn reaches out to Ed and Lorraine Warren, world renound paranormal investigators and demonologists. With the Warren's help, they discover that the entity in their house is evil, and set to work saving the family."})
+# movie = Movie.create!({title: "The Conjuring", year: 2013, plot: "When the Perron family movies into a new house, they quickly start experiencing paranormal happenings. Things keep getting worse, so mom Carolyn reaches out to Ed and Lorraine Warren, world renound paranormal investigators and demonologists. With the Warren's help, they discover that the entity in their house is evil, and set to work saving the family."})
+
+user = User.create(name: "nemo", email: "nemo@gmail.com", password: "chicken", password_confirmation: "chicken")
