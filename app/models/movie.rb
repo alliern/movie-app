b73a998e9d992acd52647e0e84d423488b862ab8
Allie Neal <allieneal93@gmail.com>
2021-03-15 11:01:20 -0400

    adds through associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,16 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+  
   # validates :movie, :title, presence: true
   # validates :movie, :plot, length: {minimum: 10}
   # validates :movie, :plot, :presence: true
   # validates :movie, :year, numericality: {only_integer: true}
+
+  def genre_names
+    @names = Genre.all
+    names.name 
+  end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,15 @@
 
 # movie = Movie.create!({title: "The Conjuring", year: 2013, plot: "When the Perron family movies into a new house, they quickly start experiencing paranormal happenings. Things keep getting worse, so mom Carolyn reaches out to Ed and Lorraine Warren, world renound paranormal investigators and demonologists. With the Warren's help, they discover that the entity in their house is evil, and set to work saving the family."})
 
-user = User.create(name: "nemo", email: "nemo@gmail.com", password: "chicken", password_confirmation: "chicken")
+# user = User.create(name: "nemo", email: "nemo@gmail.com", password: "chicken", password_confirmation: "chicken")
+
+# genre = Genre.create(name: "thriller")
+# genre = Genre.create(name: "drama")
+# genre = Genre.create(name: "horror")
+# genre = Genre.create(name: "comedy")
+
+movie_genre = MovieGenre.create(genre_id: 2, movie_id: 1)
+movie_genre = MovieGenre.create(genre_id: 2, movie_id: 2)
+movie_genre = MovieGenre.create(genre_id: 4, movie_id: 3)
+movie_genre = MovieGenre.create(genre_id: 1, movie_id: 4)
+
